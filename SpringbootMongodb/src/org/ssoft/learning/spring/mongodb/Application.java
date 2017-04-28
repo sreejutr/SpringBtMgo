c24af2a36c40b2c0e1ea5cb6d1c17415522bcdd4
sreeju <sreejutr@gmail.com>
2017-04-27 21:07:27 -0400

    comment line added

--- SpringbootMongodb/src/org/ssoft/learning/spring/mongodb/Application.java
+++ SpringbootMongodb/src/org/ssoft/learning/spring/mongodb/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.ssoft.learning.spring.mongodb.domain.Customer;
 import org.ssoft.learning.spring.mongodb.repo.CustomerRepository;
-
+//comments
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
